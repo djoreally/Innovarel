33f954176bd784d6e948334d2ce71d6dd476e4ba
JaznanOfficial <jaznanofficial@gmail.com>
2024-05-14 20:04:08 +0600

    card skeleton design changed

--- src/components/skeletons/SingleProjectSkeleton.js
+++ src/components/skeletons/SingleProjectSkeleton.js
@@ -4,10 +4,22 @@ const SingleProjectSkeleton = () => {
     return (
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-8 mt-10">
             {Array.from({ length: 6 }).map((_, index) => (
-                <div className="relative w-full mx-auto animate-pulse" key={index}>
-                    <div className="bg-gray-300 rounded-md h-96"></div>
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <div className="h-8 bg-gray-400 rounded w-24"></div>
+                <div
+                    className=" w-full animate-pulse shadow-lg bg-[#3A2B71] rounded-lg p-5 flex flex-col justify-between gap-3"
+                    key={i}
+                >
+                    <div className="space-y-2 w-full">
+                        <div className="h-6 bg-gray-400 rounded w-1/2 mx-auto"></div>
+                        <div className="h-6 bg-gray-400 rounded w-1/3 mx-auto"></div>
+                        <div className="h-24 bg-gray-400 rounded w-full"></div>
+                    </div>
+
+                    <div className="w-full h-80 rounded-lg bg-gray-300"></div>
+
+                    <div className="flex flex-row items-center justify-center gap-2 w-full">
+                        <div className="h-6 rounded bg-gray-400 w-24"></div>
+                        <div className="h-6 rounded bg-gray-400 w-24"></div>
+                        <div className="h-6 rounded bg-gray-400 w-24"></div>
                     </div>
                 </div>
             ))}
